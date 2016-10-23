03655bddd26526372c852be664e0e885f5b4712f
Tyler Berry <tyler@berryac.com>
2016-10-23 13:46:45 -0600

    bit_ops more comprehensive, with tests

--- src/bit_ops.rs
+++ src/bit_ops.rs
@@ -1,3 +1,53 @@
-pub fn toggle_bit(num: u32, bit_index: usize) -> u32 {
-    num ^ (1 << bit_index)
+/// `start` and `end` are 0-based from LSB (least significant bit).
+/// `start` is inclusive and `end` is exclusive.
+pub fn get_bits(bits: u32, start: usize, end: usize) -> u32 {
+    // !0 is equal to 0xffffffff
+    let mask = !(!0 << (end - start + 1));
+    (bits >> start) & mask
+}
+
+pub fn set_bit(bits: u32, bit_index: usize) -> u32 {
+    let mask = 1 << bit_index;
+    bits | mask
+}
+
+pub fn clear_bit(bits: u32, bit_index: usize) -> u32 {
+    let mask = !(1 << bit_index);
+    bits & mask
+}
+
+pub fn toggle_bit(bits: u32, bit_index: usize) -> u32 {
+    let mask = 1 << bit_index;
+    bits ^ mask
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_bits() {
+        assert_eq!(0xff, get_bits(0xff000000, 24, 32));
+    }
+
+    #[test]
+    fn test_set_bit() {
+        assert_eq!(0b1011, set_bit(0b1010, 0));
+        assert_eq!(0b1011, set_bit(0b1011, 0));
+        assert_eq!(0b1010, set_bit(0b0010, 3));
+    }
+
+    #[test]
+    fn test_clear_bit() {
+        assert_eq!(0b1010, clear_bit(0b1011, 0));
+        assert_eq!(0b1010, clear_bit(0b1010, 0));
+        assert_eq!(0b0010, clear_bit(0b1010, 3));
+    }
+
+    #[test]
+    fn test_toggle_bit() {
+        assert_eq!(0b1011, toggle_bit(0b1010, 0));
+        assert_eq!(0b1010, toggle_bit(0b1011, 0));
+        assert_eq!(0b1010, toggle_bit(0b0010, 3));
+    }
 }
