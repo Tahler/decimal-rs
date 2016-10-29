4e408ea9d6787b51653c90db478b01e82be552f7
Tyler Berry <tyler@berryac.com>
2016-10-29 13:46:09 -0600

    adjusted bit_ops to be inclusive, added more docs

--- src/bit_ops.rs
+++ src/bit_ops.rs
@@ -1,21 +1,39 @@
-/// `start` and `end` are 0-based from LSB (least significant bit).
-/// `start` is inclusive and `end` is exclusive.
+/// Returns the range of bits from index `start` to index `end`.
+///
+/// `start` and `end` are both inclusive and 0-based from LSB (least significant bit).
 pub fn get_bits(bits: u32, start: usize, end: usize) -> u32 {
     // !0 is equal to 0xffffffff
-    let mask = !(!0 << (end - start + 1));
+    let range = (end + 1) - start;
+    let mask = !(!0 << range);
     (bits >> start) & mask
 }
 
+/// Returns the value of the bit at `bit_index`.
+///
+/// `bit_index` is 0-based from LSB (least significant bit).
+pub fn get_bit(bits: u32, bit_index: usize) -> u32 {
+    get_bits(bits, bit_index, bit_index)
+}
+
+/// Sets the bit at `bit_index` to 1.
+///
+/// `bit_index` is 0-based from LSB (least significant bit).
 pub fn set_bit(bits: u32, bit_index: usize) -> u32 {
     let mask = 1 << bit_index;
     bits | mask
 }
 
+/// Sets the bit at `bit_index` to 0.
+///
+/// `bit_index` is 0-based from LSB (least significant bit).
 pub fn clear_bit(bits: u32, bit_index: usize) -> u32 {
     let mask = !(1 << bit_index);
     bits & mask
 }
 
+/// Flips the bit at `bit_index` to 0.
+///
+/// `bit_index` is 0-based from LSB (least significant bit).
 pub fn toggle_bit(bits: u32, bit_index: usize) -> u32 {
     let mask = 1 << bit_index;
     bits ^ mask
@@ -27,7 +45,20 @@ mod tests {
 
     #[test]
     fn test_get_bits() {
-        assert_eq!(0xff, get_bits(0xff000000, 24, 32));
+        assert_eq!(0xff, get_bits(0xff000000, 24, 31));
+        assert_eq!(0b_101, get_bits(0b110101, 0, 2));
+
+        let bit_32 = 0b1_00111010_01101000110011111000000;
+        assert_eq!(0b1, get_bits(bit_32, 31, 31));
+        assert_eq!(0b1, get_bit(bit_32, 31));
+        assert_eq!(0b00111010, get_bits(bit_32, 23, 30));
+        assert_eq!(0b01101000110011111000000, get_bits(bit_32, 0, 22));
+
+        let bit_32 = 0b0_01011110_11001011011100110101010;
+        assert_eq!(0b0, get_bits(bit_32, 31, 31));
+        assert_eq!(0b0, get_bit(bit_32, 31));
+        assert_eq!(0b01011110, get_bits(bit_32, 23, 30));
+        assert_eq!(0b11001011011100110101010, get_bits(bit_32, 0, 22));
     }
 
     #[test]
